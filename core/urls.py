b6e04073047e1579e4d52ef1f2cb49ad5d62b9e0
jejeV <muejason27@gmail.com>
2023-01-30 17:42:13 +0700

    auth

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('', views.index, name='index'),
-      path('signin', views.signin, name='signin'),
-      path('signup', views.singup, name='singup'),
+    path('', views.index, name='index'),
+    path('signup', views.singup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -59,3 +59,7 @@ def signin(request):
 
     else:
         return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')

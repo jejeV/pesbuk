c348b9f1ea71eb14627aee82b229f857b5a0895f
jejeV <muejason27@gmail.com>
2023-01-31 12:19:16 +0700

    settings

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
     path('signup', views.singup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
